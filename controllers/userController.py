cfd5cd2f180c3774ce25f2bdec0b37111e01479f
mohammad alaie <mohammad.alaie.work@gmail.com>
2025-07-22 23:01:59 +0330

    adding Crud to project

--- controllers/userController.py
+++ controllers/userController.py
@@ -7,3 +7,35 @@ def save_user(userData):
         return {"job":"ok", "data":str(created_user)}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+
+def delete_user_with_id(userId):
+    deleted_user = User.objects(id=userId).delete()
+    if deleted_user == 0:
+        raise HTTPException(status_code=404, detail="User not found")
+    else:
+        return {"job":"ok", "data":f"user with id {userId} deleted successfully"}
+
+def find_users(page,limit):
+    skip = (page-1)*limit
+    userList = list(User.objects().skip(skip).limit(limit).as_pymongo())
+    if len(userList) == 0:
+        raise HTTPException(status_code=204, detail="No users found")
+    else:
+        for user in userList:
+            user["_id"] = str(user["_id"])
+        return {"job":"ok", "data":userList}
+    
+def find_user_with_id(userId):
+    founded_user = list(User.objects(id=userId).as_pymongo())[0]
+    if founded_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    else:
+        founded_user["_id"] = str(founded_user["_id"])
+        return {"job":"ok", "data":founded_user}
+def update_user_with_id(userId, newUserData):
+    founded_user = User.objects(id=userId).update(**newUserData)
+    if founded_user == 0:
+        raise HTTPException(status_code=404, detail="User not found")
+    else:
+        return {"job":"ok", "data":f"user with id {userId} updated successfully"}
--- routes/userRouter.py
+++ routes/userRouter.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter,Depends
 from validations.userValidator import userValidator,getUserData
-from controllers.userController import save_user
+from controllers.userController import update_user_with_id,find_user_with_id,find_users,save_user,delete_user_with_id
 
 user_router = APIRouter()
 
 
 @user_router.get('/users')
-async def get_all_users():
-    return {"message": "All users"}
+async def get_all_users(page:int=1,limit:int=10):
+    return find_users(page,limit)
 
 @user_router.get('/users/{user_id}')
-async def get_user(user_id: int):
-    return {"message": f"User with id {user_id}"}
+async def get_user(user_id: str):
+    return find_user_with_id(user_id)
 
 @user_router.post('/users')
 async def create_user(userData:userValidator = Depends(getUserData)):
@@ -19,13 +19,10 @@ async def create_user(userData:userValidator = Depends(getUserData)):
     
 
 @user_router.put('/users/{user_id}')
-async def update_user(user_id: int):
-    return {"message": f"User with id {user_id} updated"}
+async def update_user(user_id: str,userData:userValidator = Depends(getUserData)):
+    return update_user_with_id(user_id,dict(userData))
 
 @user_router.delete('/users/{user_id}')
-async def delete_user(user_id: int):
-    return {"message": f"User with id {user_id} deleted"}
+async def delete_user(user_id: str):
+    return delete_user_with_id(user_id)
 
-@user_router.post("/users/reset-password")
-async def reset_password():
-    return {"message":"password reset"}
